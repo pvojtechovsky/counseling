fc1780643c8fa77c3a7aa151982e42cc2f8c74f2
Pavel Vojtěchovský <pavel.vojtechovsky@usu.com>
2024-08-01 18:34:08 +0200

    quartz config

---
+++ .quartz-patch/quartz.layout.ts
@@ -0,0 +1,50 @@
+import { PageLayout, SharedLayout } from "./quartz/cfg"
+import * as Component from "./quartz/components"
+
+// components shared across all pages
+export const sharedPageComponents: SharedLayout = {
+  head: Component.Head(),
+  header: [],
+  afterBody: [],
+  footer: Component.Footer({
+    links: {
+      "Kontakt": "https://www.facebook.com/pavel.vojtechovsky.92/",
+      "Psychofonetika": "https://skolaempatie.sk/o-nas/o-psychofonetike/",
+    },
+  }),
+}
+
+// components for pages that display a single page (e.g. a single note)
+export const defaultContentPageLayout: PageLayout = {
+  beforeBody: [
+    Component.Breadcrumbs({rootName: "Domů"}),
+    Component.ArticleTitle(),
+    Component.ContentMeta(),
+    Component.TagList(),
+  ],
+  left: [
+    Component.PageTitle(),
+    Component.MobileOnly(Component.Spacer()),
+    Component.Search(),
+    Component.Darkmode(),
+    Component.DesktopOnly(Component.Explorer({folderClickBehavior: "link"})),
+  ],
+  right: [
+    // Component.Graph(),
+    Component.DesktopOnly(Component.TableOfContents()),
+    // Component.Backlinks(),
+  ],
+}
+
+// components for pages that display lists of pages  (e.g. tags or folders)
+export const defaultListPageLayout: PageLayout = {
+  beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta()],
+  left: [
+    Component.PageTitle(),
+    Component.MobileOnly(Component.Spacer()),
+    Component.Search(),
+    Component.Darkmode(),
+    Component.DesktopOnly(Component.Explorer()),
+  ],
+  right: [],
+}
